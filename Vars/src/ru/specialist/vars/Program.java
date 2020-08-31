6183dbae2cba1a21a64c895271fc327300ee3545
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-08-31 22:31:27 +0300

    Add files via upload

---
+++ Vars/src/ru/specialist/vars/Program.java
@@ -0,0 +1,86 @@
+package ru.specialist.vars;
+
+//import java.util.Date;
+//import java.util.Calendar;
+//import java.util.*;
+//import java.sql.*;
+import java.io.BufferedReader; 
+import java.util.Calendar;
+import java.util.Date;
+
+//import static java.lang.Math.PI;
+//import static java.lang.Math.sin;
+//import static java.lang.Math.cos;
+import static java.lang.Math.*;
+//import com.mysql.jdbc.Driver;
+
+public class Program {
+	
+	public static int x;
+	
+	int y;
+
+	
+	public static void main(String[] args) {
+//		goto
+		
+		
+//		java.util.Date d;
+		Date d;
+		Calendar c;
+		BufferedReader b2;
+		
+		int k = 2;
+		final double q = k;
+		
+		double r = cos(sin(PI * q));
+		
+		int k = 4;
+		Integer k2 = k; // boxing
+		Integer k1 = k; 
+		
+		Integer k4 = k1 + k2;
+		
+		int k3 = k2; // unboxing
+		
+		System.out.println(r);
+		
+		{
+			int z = 10;
+			System.out.println(z);
+		}
+		{
+			String z = "abc";
+			System.out.println(z);
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		int counter;
+		String userName = "Roman";
+		int a = 2,b = 3;
+		
+		String s;
+		
+		counter = 10 + 1;
+		
+		System.out.println(x);
+		
+		
+		System.out.println(counter);
+		System.out.println(userName);
+		
+		
+		//Math.PI
+	}
+}
