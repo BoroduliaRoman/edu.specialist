d4022317997be97789f7817a2c4eaa6c2bb90f33
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-12-16 15:18:10 +0200

    Resources

---
+++ Resources/src/DBConnection.java
@@ -0,0 +1,29 @@
+
+
+public class DBConnection implements AutoCloseable {
+	public DBConnection() {
+		System.out.println("Open connection...");
+	}
+	
+	public void execute() {
+		System.out.println("execute command");
+	}
+
+	private boolean isClosed = false;
+	
+	@Override
+	public void close() {
+		if (!isClosed) {
+			System.out.println("close connection");
+			isClosed = true;
+		}
+	}
+	
+	/*
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		System.out.println("close connection");
+	}
+	*/
+}
